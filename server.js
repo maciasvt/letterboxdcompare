4a8907594bc86f2886a42790d7ff86520472c2e6
maciasvt <48213702+maciasvt@users.noreply.github.com>
2025-08-08 17:13:23 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Permitir todas las cabeceras y métodos
 app.use(cors({
     origin: 'https://maciasvt.github.io', // Permitir solicitudes de este origen
+    //origin:'http://127.0.0.1:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true // Si tu API necesita cookies, etc.
 }));
@@ -20,6 +21,7 @@ app.get('/extraer', async (req, res) => {
         const html = response.data;
 
         res.setHeader('Access-Control-Allow-Origin', 'https://maciasvt.github.io'); // Agregar manualmente
+        //res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:3001');
         res.send(html);
     } catch (error) {
         res.status(500).send('Error al extraer la página: ' + error.message);
