a492945953bf221b781b49068c075131072cb405
maciasvt <48213702+maciasvt@users.noreply.github.com>
2025-08-08 09:05:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const app = express();
-const PORT = process.env.PORT || 3000; // Cambiamos la forma de utilizar el puerto
+const cors = require('cors'); // Importar cors
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // Habilitar CORS para todas las rutas
 app.use(express.static('public')); // Carpeta para archivos estáticos
 
 app.get('/extraer', async (req, res) => {
-    const { url } = req.query; // Obtén la URL de la consulta
+    const { url } = req.query; 
     try {
         const response = await axios.get(url);
         const html = response.data;
-        res.send(html);
+        res.send(html); // O puedes enviar respuesta coherente como arriba
     } catch (error) {
         res.status(500).send('Error al extraer la página: ' + error.message);
     }
 });
 
-app.listen(PORT, () => { // Cambiado a usar PORT
+app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
